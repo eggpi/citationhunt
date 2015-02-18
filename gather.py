c06f2b74ffd02d1a03d3d8efe4654ce9380e9468
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2015-02-18 09:43:35 -0200

    Refactor constants and db creation.

--- gather.py
+++ gather.py
@@ -11,18 +11,25 @@ WIKIPEDIA_BASE_URL = 'https://en.wikipedia.org'
 WIKIPEDIA_WIKI_URL = WIKIPEDIA_BASE_URL + '/wiki/'
 WIKIPEDIA_API_URL = WIKIPEDIA_BASE_URL + '/w/api.php'
 
+MARKER = '7b94863f3091b449e6ab04d44cb372a0' # unlikely to be in any article
+CITATION_NEEDED_HTML = '<span class="citation-needed">[citation needed]</span>'
+
+def init_db():
+    db = sqlite3.connect('citationhunt.sqlite3')
+    cursor = db.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS cn (snippet text, url text, title text)
+    ''')
+
+    return db, cursor
+
+def is_citation_needed(t):
+    return t.name.matches('Citation needed') or t.name.matches('cn')
+
+db, cursor = init_db()
 wikipedia = wikitools.wiki.Wiki(WIKIPEDIA_API_URL)
 category = wikitools.Category(wikipedia, 'All_articles_with_unsourced_statements')
-marker = '7b94863f3091b449e6ab04d44cb372a0' # unlikely to be in any article
-citation_needed_html = '<span class="citation-needed">[citation needed]</span>'
-
-db = sqlite3.connect('citationhunt.sqlite3')
-cursor = db.cursor()
-
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS cn (snippet text, url text, title text)
-''')
-
 for page in category.getAllMembersGen():
     wikitext = page.getWikiText()
 
@@ -30,7 +37,7 @@ for page in category.getAllMembersGen():
         wikicode = mwparserfromhell.parse(paragraph)
 
         for t in wikicode.filter_templates():
-            if t.name.matches('cn') or t.name.matches('Citation needed'):
+            if is_citation_needed(t):
                 stripped_len = len(wikicode.strip_code())
                 if stripped_len > 420 or stripped_len < 50:
                     # TL;DR or too short
@@ -38,9 +45,9 @@ for page in category.getAllMembersGen():
 
                 # add the marker so we know where the Citation-needed template
                 # was, and remove all markup (including the template)
-                wikicode.insert_before(t, marker)
+                wikicode.insert_before(t, MARKER)
                 snippet = wikicode.strip_code()
-                snippet = snippet.replace(marker, citation_needed_html)
+                snippet = snippet.replace(MARKER, CITATION_NEEDED_HTML)
 
                 url = WIKIPEDIA_WIKI_URL + urlparse.unquote(page.urltitle)
                 url = unicode(url, 'utf-8')
