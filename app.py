a36a4a73f7a6a2b7878fe3807c48519cad8428b0
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2015-02-19 12:04:15 -0200

    Add link to Referencing_for_beginners
    Former-commit-id: b6445a3d5d594b7a53894ffcd26f2242a22367b8

--- app.py
+++ app.py
@@ -48,5 +48,4 @@ def close_db(exception):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     debug = 'DEBUG' in os.environ
-    print debug
     app.run(host = '0.0.0.0', port = port, debug = debug)
