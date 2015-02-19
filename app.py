ffff0cb075b42e64978465e1d3db916d4e634008
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2015-02-19 09:39:41 -0200

    Read DEBUG off of the environment.

--- app.py
+++ app.py
@@ -48,4 +48,6 @@ def close_db(exception):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host = '0.0.0.0', port = port)
+    debug = 'DEBUG' in os.environ
+    print debug
+    app.run(host = '0.0.0.0', port = port, debug = debug)
