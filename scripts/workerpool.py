bf63b397b85eae727fdf007be28dc47c34d149c1
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2015-02-22 16:49:30 -0300

    Document WorkerPool.

--- scripts/workerpool.py
+++ scripts/workerpool.py
@@ -2,12 +2,69 @@ import abc
 import itertools
 import multiprocessing
 
-# FIXME this can possibly be replaced with a simple multiprocessing.Pool()
-# by turning parse_xml_dump into a generator and feeding it in parts to the
-# queue using the lazy iteration trick [1]. But I didn't know that trick when
-# I wrote this, and it was fun to write anyway.
-# 1- https://stackoverflow.com/questions/5318936/python-multiprocessing-pool-lazy-iteration
 class WorkerPool(object):
+    '''
+    A pool of worker processes, somewhat similar to multiprocessing.Pool, but
+    more suitable for data generated lazily or in an event-driven manner, and
+    adopting a MapReduce-like working model.
+
+    A WorkerPool consists of multiprocessing.cpu_count() processes, one of
+    which being the receiver (which is similar to a reducer), and the others
+    being workers (which are analogous to mappers). The receiver and the
+    workers must conform to the Receiver and Worker interfaces defined by this
+    module, which consist of the setup(), work()/receive() and done() methods.
+
+    The setup() method will get called right after forking, but before any tasks
+    are sent to the workers and receiver. Likewise, the done() method will get
+    called when there are no more tasks to be processed, as signaled by the
+    application by calling WorkerPool.done(), first in all the workers, then in
+    the receiver when all worker processes have finished.
+
+    Before WorkerPool.done() is called, the work() method in workers will get
+    called with whatever object is passed to WorkerPool.post(), and its return
+    value is passed to the receiver's receive() method. This model makes
+    WorkerPool suitable for large datasets that are generated lazily or via
+    event-driven libraries.
+
+    For example, here's how WorkerPool can be used to process a big XML file
+    such that the parent process parses it, the workers process the data it
+    contains, and the receiver saves the results to a database, and the file is
+    never entirely loaded in memory:
+
+        import xml.etree.ElementTree as ET
+
+        class DataParser(workerpool.Worker):
+            def setup():
+                pass
+
+            def work(data):
+                result = self.do_stuff_with_data(data)
+                return result
+
+            def done():
+                pass
+            ...
+
+        class DataWriter(workerpool.Receiver):
+            def setup():
+                self.db = self.connect_to_database()
+
+            def receive(result):
+                self.write_to_database(db, result)
+
+            def done():
+                pass
+            ...
+
+        wp = WorkerPool(DataParser(), DataWriter())
+        iterparser = ET.iterparse(large_xml_file)
+        for _, element in iterparser:
+            data = element.find('data').text
+            wp.post(data)
+            element.clear()
+        wp.done()
+    '''
+
     def __init__(self, worker, receiver):
         self._procs = []
         self._queues = []
