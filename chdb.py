35377be80ecb757e22a5cbdde0c345c85d971709
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2015-02-24 10:20:47 -0300

    Cascade article deletion.

--- chdb.py
+++ chdb.py
@@ -14,7 +14,7 @@ def init_db():
             DROP TABLE IF EXISTS snippets
         ''')
         db.execute('''
-            CREATE TABLE categories (page_id TEXT PRIMARY KEY, title TEXT)
+            CREATE TABLE categories (id TEXT PRIMARY KEY, title TEXT)
         ''')
         db.execute('''
             INSERT INTO categories VALUES ("unassigned", "unassigned")
@@ -22,12 +22,13 @@ def init_db():
         db.execute('''
             CREATE TABLE articles (page_id TEXT PRIMARY KEY, url TEXT,
             title TEXT, category_id TEXT,
-            FOREIGN KEY(category_id) REFERENCES categories(page_id))
+            FOREIGN KEY(category_id) REFERENCES categories(id))
+            ON DELETE CASCADE
         ''')
         db.execute('''
             CREATE TABLE snippets (id TEXT PRIMARY KEY, snippet TEXT,
             article_id TEXT, FOREIGN KEY(article_id)
-            REFERENCES articles(page_id))
+            REFERENCES articles(page_id)) ON DELETE CASCADE
         ''')
 
     return db
