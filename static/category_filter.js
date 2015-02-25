3d0ffd0700305dc85ccf6adc83d99e73c279f419
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2015-02-25 13:35:30 -0300

    Move category filter to a separate script.
    Former-commit-id: 0c288cd6f168235f749eed3c0c57a3990ac46f32

---
+++ static/category_filter.js
@@ -0,0 +1,53 @@
+function initCategoryFilter(currentCategoryId) {
+  var cin = document.getElementById("category-input");
+  var chi = document.getElementById("hidden-category-input");
+  var options = document.getElementsByTagName("option");
+
+  for (var i = 0; i < options.length; i++) {
+    if (options[i].value === currentCategoryId) {
+      cin.value = options[i].label;
+      chi.value = options[i].value;
+    }
+  }
+
+  function filterEmptyMatchesAll(text, input) {
+    return (input === "") || Awesomplete.FILTER_CONTAINS(text, input);
+  }
+
+  var awc = new Awesomplete(cin);
+  awc.minChars = 0;
+  awc.maxItems = options.length;
+  awc.sort = undefined;
+
+  cin.addEventListener('click', function() {
+    awc.evaluate();
+  });
+
+  cin.addEventListener("awesomplete-open", function() {
+    this.classList.add("open");
+  })
+
+  cin.addEventListener("awesomplete-close", function() {
+    this.classList.remove("open");
+  })
+
+  function set_hidden_category() {
+    var catname = cin.value.toLocaleLowerCase();
+
+    for (var i = 0; i < options.length; i++) {
+      if (options[i].label.toLocaleLowerCase() == catname) {
+        chi.value = options[i].value;
+      }
+    }
+  }
+
+  cin.addEventListener("awesomplete-selectcomplete", function() {
+    set_hidden_category();
+    this.form.submit();
+  });
+
+  cin.form.addEventListener("submit", function() {
+    set_hidden_category();
+    return true;
+  });
+}
