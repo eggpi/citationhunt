9b3856702638a9246a1a977bf1902777a068a578
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2015-04-05 12:20:15 -0300

    Email at the end of database updates.

--- scripts/update_db_tools_labs.sh
+++ scripts/update_db_tools_labs.sh
@@ -1,4 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+
+function email() {
+    echo "The logs are attached." | \
+        /usr/bin/mail -s "$1" -a ~/update_db_tools_labs.err \
+        citationhunt.update@tools.wmflabs.org
+    sleep 2m
+}
+
+truncate -s 0 update_db_tools_labs.err
 
 . ~/www/python/venv/bin/activate
 cd ~/www/python/src/scripts/
@@ -20,10 +29,23 @@ echo >&2 ":: latest dump is $dump_date"
 
 echo >&2 ":: generating unsourced pageids"
 ./print_unsourced_pageids_from_wikipedia > unsourced
+if [ $? -ne 0 ]; then
+    email "Failed at print_unsourced_pageids_from_wikipedia."
+    exit 1
+fi
 echo >&2 ":: parsing pages-articles.xml.bz2"
 ./parse_pages_articles.py $dump_dir/enwiki-$dump_date-pages-articles.xml.bz2 unsourced
+if [ $? -ne 0 ]; then
+    email "Failed at parse_pages_articles.py"
+    exit 1
+fi
 echo >&2 ":: assigning categories"
 ./assign_categories.py --max-categories=5500 --mysql-config="$mysql_cnf"
+if [ $? -ne 0 ]; then
+    email "Failed at assign_categories.py"
+    exit 1
+fi
 
 echo >&2 ":: installing new database"
 cp citationhunt.sqlite3 ../citationhunt.sqlite3
+email "All done!"
