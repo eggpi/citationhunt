f771e2cd5bb7d5d812436857deda00a0f574a435
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2016-01-22 18:13:21 +0000

    Fix handling of refnec params, add test.

---
+++ snippet_parser/fr_test.py
@@ -0,0 +1,30 @@
+#-*- encoding: utf-8 -*-
+from __future__ import unicode_literals
+
+import os
+os.environ['CH_LANG'] = 'fr'
+from base import *
+snippet_parser = get_localized_snippet_parser()
+
+import unittest
+import functools
+
+# ignore size limits to make these tests
+extract_snippets = functools.partial(
+    snippet_parser.extract_snippets, minlen = 0, maxlen = float('inf'))
+
+def extract_lead_snippets(text):
+    snippets = extract_snippets(text)
+    assert snippets[0][0] == ''
+    return snippets[0][1]
+
+class ExtractSnippetTest(unittest.TestCase):
+    def test_refnec_args(self):
+        s = '''En {{refnec|date=mai 2011|1693}}, il fait la connaissance de [[Gottfried Wilhelm Leibniz|Leibniz]].'''
+        self.assertEqual(
+            extract_lead_snippets(s),
+            ['En 1693%s, il fait la connaissance de Leibniz.' %
+                CITATION_NEEDED_MARKER])
+
+if __name__ == '__main__':
+    unittest.main()
