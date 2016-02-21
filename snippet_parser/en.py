c655841f303b687c0ed46f5c5a48128a30e82f3b
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2016-02-21 08:41:33 +0000

    Implement the flag template.
    Former-commit-id: 650266661a3afe242f8533d6a075d7b494e574ba

--- snippet_parser/en.py
+++ snippet_parser/en.py
@@ -1,9 +1,16 @@
 from base import *
 
+def handle_flag(template):
+    if template.has('name'):
+        return sp(template.get('name'))
+    return sp(template.params[0])
+
 class SnippetParser(SnippetParserBase):
     def strip_template(self, template, normalize, collapse):
         if template.name.matches('convert'):
             return ' '.join(sp(template.params[:2]))
+        elif template.name.matches('flag'):
+            return handle_flag(template)
         elif self.is_citation_needed(template):
             return CITATION_NEEDED_MARKER
         return ''
