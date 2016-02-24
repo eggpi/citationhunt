6057ac75e6f3143f8910b63426be835d68700ebe
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2016-02-24 20:08:22 +0000

    Implement bandiera and citazione for Italian.
    Part of #36.

--- snippet_parser/it.py
+++ snippet_parser/it.py
@@ -3,5 +3,18 @@ from __future__ import unicode_literals
 
 from base import *
 
+def handle_bandiera(template):
+    return template.get(1)
+
+def handle_citazione(template):
+    if template.params:
+        return '« ' + sp(template.params[0]) + ' »'
+
 class SnippetParser(SnippetParserBase):
-    pass
+    def strip_template(self, template, normalize, collapse):
+        if template.name.matches('bandiera'):
+            return handle_bandiera(template)
+        elif template.name.matches('citazione'):
+            return handle_citazione(template)
+        return super(SnippetParser, self).strip_template(
+                template, normalize, collapse)
