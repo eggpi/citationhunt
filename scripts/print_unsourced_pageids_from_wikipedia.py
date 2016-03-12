fc2b53ad3d9264558e104e7eba4b6e9f460521e2
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2016-03-12 15:07:14 +0000

    Query the category for unsourced articles recursively.
    Former-commit-id: b5983f53c57ca18bb41599cc741f06130189dcd6

--- scripts/print_unsourced_pageids_from_wikipedia.py
+++ scripts/print_unsourced_pageids_from_wikipedia.py
@@ -9,7 +9,25 @@ import config
 cfg = config.get_localized_config()
 db = chdb.init_wp_replica_db()
 cursor = db.cursor()
-cursor.execute('SELECT cl_from FROM categorylinks WHERE cl_to = %s',
-    (cfg.citation_needed_category,))
-for row in cursor:
-    print row[0]
+
+categories = set([cfg.citation_needed_category])
+while True:
+    cursor.execute(
+        'SELECT cl_from, cl_type FROM categorylinks WHERE (' +
+        ' OR '.join(['cl_to = %s'] * len(categories)) + ')', categories)
+    subcategories = set()
+    for page_id, type in cursor:
+        if type == 'page':
+            print page_id
+        elif type == 'subcat':
+            subcategories.add(page_id)
+    if not subcategories:
+        break
+
+    # need to convert the page ids of subcategories into page
+    # titles so we can query recursively
+    cursor.execute(
+        'SELECT page_title FROM page WHERE (' +
+        ' OR '.join(['page_id = %s'] * len(subcategories)) + ')',
+        subcategories)
+    categories = set([r[0] for r in cursor])
