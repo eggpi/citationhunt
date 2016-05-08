4aca2e64b546755bdf8a5fbb90a03f938e10fa5e
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2016-05-08 02:30:52 +0100

    Get rid of update_db_tools_labs.sh

--- utils.py
+++ utils.py
@@ -15,6 +15,9 @@ def d(s):
 def mkid(s):
     return hashlib.sha1(e(s)).hexdigest()[:2*4]
 
+def running_in_virtualenv():
+    return hasattr(sys, 'real_prefix')
+
 def running_in_tools_labs():
     return os.path.exists('/etc/wmflabs-project')
 
