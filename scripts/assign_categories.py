541d7c6c5940a9ace26874403ab9ab8d7f220d1e
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2016-05-08 02:55:54 +0100

    Properly set up sys.path in scripts.

--- scripts/assign_categories.py
+++ scripts/assign_categories.py
@@ -13,8 +13,12 @@ Options:
 
 from __future__ import unicode_literals
 
+import os
 import sys
-sys.path.append('../')
+_upper_dir = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..'))
+if _upper_dir not in sys.path:
+    sys.path.append(_upper_dir)
 
 import config
 import chdb as chdb_
