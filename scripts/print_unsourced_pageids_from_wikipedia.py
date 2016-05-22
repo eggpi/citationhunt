bb6aa857269675381065616a9469367187987fe8
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2016-05-22 02:12:36 +0100

    Syntax-check scripts in the commit hook.

--- scripts/print_unsourced_pageids_from_wikipedia.py
+++ scripts/print_unsourced_pageids_from_wikipedia.py
@@ -10,28 +10,31 @@ if _upper_dir not in sys.path:
 import chdb
 import config
 
-cfg = config.get_localized_config()
-db = chdb.init_wp_replica_db()
-cursor = db.cursor()
+def print_unsourced_ids_from_wikipedia():
+    cfg = config.get_localized_config()
+    db = chdb.init_wp_replica_db()
+    cursor = db.cursor()
+    categories = set([cfg.citation_needed_category])
+    while True:
+        cursor.execute(
+            'SELECT cl_from, cl_type FROM categorylinks WHERE (' +
+            ' OR '.join(['cl_to = %s'] * len(categories)) + ')', categories)
+        subcategories = set()
+        for page_id, type in cursor:
+            if type == 'page':
+                print page_id
+            elif type == 'subcat':
+                subcategories.add(page_id)
+        if not subcategories:
+            break
 
-categories = set([cfg.citation_needed_category])
-while True:
-    cursor.execute(
-        'SELECT cl_from, cl_type FROM categorylinks WHERE (' +
-        ' OR '.join(['cl_to = %s'] * len(categories)) + ')', categories)
-    subcategories = set()
-    for page_id, type in cursor:
-        if type == 'page':
-            print page_id
-        elif type == 'subcat':
-            subcategories.add(page_id)
-    if not subcategories:
-        break
+        # need to convert the page ids of subcategories into page
+        # titles so we can query recursively
+        cursor.execute(
+            'SELECT page_title FROM page WHERE (' +
+            ' OR '.join(['page_id = %s'] * len(subcategories)) + ')',
+            subcategories)
+        categories = set([r[0] for r in cursor])
 
-    # need to convert the page ids of subcategories into page
-    # titles so we can query recursively
-    cursor.execute(
-        'SELECT page_title FROM page WHERE (' +
-        ' OR '.join(['page_id = %s'] * len(subcategories)) + ')',
-        subcategories)
-    categories = set([r[0] for r in cursor])
+if __name__ == '__main__':
+    print_unsourced_ids_from_wikipedia()
