ea6ff58b79f1ac99f992b24eb1a976c06b32a915
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2016-06-06 20:56:43 +0100

    Run import checks for every language.

--- run-tests.sh
+++ run-tests.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-find . -name '*_test.py' -print0 | xargs -0 -n1 python
+ret=0
 
-# I'm too lazy to write tests for most of the scripts,
-# and they usually break very obviously on Tools Labs anyway,
-# so just try importing them to do a quick syntax check
-for s in scripts/*.py; do
-    echo "import-checking $s..."
-    importable="$(echo "$s" | sed -E 's!scripts/(.*)\.py!\1!')"
-    CH_LANG=en PYTHONPATH=$PWD/scripts/ python -c "import $importable" && rm "${s}c"
+find . -name '*_test.py' -print0 | xargs -0 -n1 python
+[ $? -ne 0 ] && ret=1
+
+LANGS=$(python -c 'import config; print " ".join(config.lang_code_to_config)')
+for l in $LANGS; do
+    # I'm too lazy to write tests for most of the scripts,
+    # and they usually break very obviously on Tools Labs anyway,
+    # so just try importing them to do a quick syntax check
+    for s in scripts/*.py; do
+        echo "import-checking $s with CH_LANG=$l..."
+        importable="$(echo "$s" | sed -E 's!scripts/(.*)\.py!\1!')"
+        CH_LANG=$l PYTHONPATH=$PWD/scripts/ python -c "import $importable" && rm "${s}c"
+        [ $? -ne 0 ] && ret=1
+    done
 done
+
+exit $ret
