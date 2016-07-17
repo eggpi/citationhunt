9f1262736724a01bce7ae290f51c3bc64ed7e60c
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2016-07-17 17:02:50 +0100

    Bengali version: added config and crontab entry, marked WIP for now.
    Former-commit-id: bbad3fed6295d4e92418a6df0b7ccc6af230d7fa

--- config.py
+++ config.py
@@ -325,6 +325,39 @@ lang_code_to_config = dict(
 
         max_categories = 1000,
     ),
+
+    bn = dict(
+        lang_name = 'বাংলা',
+        lang_dir = 'ltr',
+        database = 'bnwiki_p',
+        wikipedia_domain = 'bn.wikipedia.org',
+
+        beginners_link =
+        'https://bn.wikipedia.org/wiki/%E0%A6%89%E0%A6%87%E0%A6%95%E0%A6%BF%E0%A6%AA%E0%A6%BF%E0%A6%A1%E0%A6%BF%E0%A6%AF%E0%A6%BC%E0%A6%BE:%E0%A6%89%E0%A7%8E%E0%A6%B8%E0%A6%A8%E0%A6%BF%E0%A6%B0%E0%A7%8D%E0%A6%A6%E0%A7%87%E0%A6%B6',
+        beginners_link_title = 'উইকিপিডিয়া:উৎসনির্দেশ',
+        lead_section_policy_link =
+        'https://es.wikipedia.org/wiki/Wikipedia:Secci%C3%B3n_introductoria#Referencia://bn.wikipedia.org/wiki/%E0%A6%89%E0%A6%87%E0%A6%95%E0%A6%BF%E0%A6%AA%E0%A6%BF%E0%A6%A1%E0%A6%BF%E0%A6%AF%E0%A6%BC%E0%A6%BE:%E0%A6%AD%E0%A7%82%E0%A6%AE%E0%A6%BF%E0%A6%95%E0%A6%BE%E0%A6%82%E0%A6%B6_%E0%A6%A8%E0%A7%80%E0%A6%A4%E0%A6%BF%E0%A6%AE%E0%A6%BE%E0%A6%B2%E0%A6%BE#.E0.A6.A4.E0.A6.A5.E0.A7.8D.E0.A6.AF.E0.A6.B8.E0.A7.82.E0.A6.A4.E0.A7.8D.E0.A6.B0',
+        lead_section_policy_link_title = 'WP:CITELEAD',
+
+        citation_needed_category = 'উৎসবিহীন_তথ্যসহ_সকল_নিবন্ধ',
+        citation_needed_templates = EN_CITATION_NEEDED_TEMPLATES + [
+            'তথ্যসূত্র প্রয়োজন',
+            'তথ্যসূত্র যাচাই',
+        ],
+        citation_needed_template_name = 'তথ্যসূত্র প্রয়োজন|',
+        wikilink_prefix_blacklist = EN_WIKILINK_PREFIX_BLACKLIST,
+        tags_blacklist = EN_TAGS_BLACKLIST,
+        templates_blacklist = EN_TAGS_BLACKLIST,
+        hidden_category = 'লুকায়িত_বিষয়শ্রেণীসমূহ',
+        category_name_regexps_blacklist = [
+            '.*[0-9]+.*',
+            '.*[০১২৩৪৫৬৭৮৯].*',
+        ],
+
+        snippet_min_size = 20,
+        max_categories = 100,
+        wip = True,
+    ),
 )
 
 # In py3: types.SimpleNamespace
