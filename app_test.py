826d59b29e40f3f3597d733020929ba1a66f3a8e
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2016-08-05 23:23:50 +0100

    Get rid of categories.html.
    Former-commit-id: 604f97ecdb0d45285d9927730cb00a69eecc86a2

--- app_test.py
+++ app_test.py
@@ -103,12 +103,6 @@ class CitationHuntTest(unittest.TestCase):
         self.assertEquals(args['cat'], app.handlers.CATEGORY_ALL.id)
 
     def test_cache_control(self):
-        response = self.app.get('/en/categories.html')
-        self.assertEquals(response.status_code, 200)
-        self.assertTrue('public' in response.cache_control)
-        self.assertEquals(
-            response.cache_control.max_age, app.CACHE_DURATION_SEMI_STATIC)
-
         response = self.app.get('/en?id=%s&cat=all' % self.sid)
         self.assertEquals(response.status_code, 200)
         self.assertTrue('public' in response.cache_control)
@@ -118,15 +112,6 @@ class CitationHuntTest(unittest.TestCase):
         response = self.app.get('/')
         self.assertEquals(response.status_code, 302)
         self.assertEquals(response.cache_control.max_age, None)
-
-    def test_gzip(self):
-        compress_min_size = app.app.config['COMPRESS_MIN_SIZE']
-        app.app.config['COMPRESS_MIN_SIZE'] = 1
-        with app.app.test_request_context('/en/categories.html'):
-            response = self.app.get('/en/categories.html',
-                headers = {'Accept-encoding': 'gzip'})
-            self.assertEquals(response.content_encoding, 'gzip')
-        app.app.config['COMPRESS_MIN_SIZE'] = compress_min_size
 
     def test_redirect(self):
         response = self.app.get('/en/redirect?to=wiki/AT%26T#History')
