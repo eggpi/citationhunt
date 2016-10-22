2cafb49daff527ab0d0be1e0e3875030c57f7b3c
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2016-10-22 11:51:12 +0100

    Add a simple footer.
    Part of #54.
    
    
    Former-commit-id: ad21f679e8af8b0086d0e5875b1b0d0b7373ef3b

--- chstrings/__init__.py
+++ chstrings/__init__.py
@@ -3,35 +3,32 @@ import flask
 import os
 import json
 
+def _link(url, title):
+    return flask.Markup(
+        '<a target="_blank" href=%s>%s</a>' % (url, title))
+
 def _preprocess_variables(config, strings):
-    in_page_link = flask.Markup(
-        '<a target="_blank" href=%s>%s</a>')
     strings['in_page'] = \
-        flask.Markup(strings['in_page']) % in_page_link
+        flask.Markup(strings['in_page']) % _link('%s', '%s')
 
     if config.lead_section_policy_link:
-        lead_section_policy_link = flask.Markup(
-            '<a target="_blank" href=%s>%s</a>') % (
+        strings['lead_section_hint'] = \
+            flask.Markup(strings['lead_section_hint']) % _link(
                 config.lead_section_policy_link,
                 config.lead_section_policy_link_title)
-        strings['lead_section_hint'] = \
-            flask.Markup(strings['lead_section_hint']) % \
-            lead_section_policy_link
     else:
         strings['lead_section_hint'] = ''
 
-    beginners_hint_link = flask.Markup(
-        '<a target="_blank" href=%s>%s</a>') % (
-            config.beginners_link,
-            config.beginners_link_title)
+    beginners_hint_link = _link(
+        config.beginners_link,
+        config.beginners_link_title)
     strings['beginners_hint'] = \
         flask.Markup(strings['beginners_hint']) % beginners_hint_link
 
     if '404' not in config.flagged_off:
-        page_not_found_link = flask.Markup('<a href=%s>Citation Hunt</a>') % (
-            config.lang_code)
         strings['page_not_found_text'] = \
-            flask.Markup(strings['page_not_found_text']) % page_not_found_link
+            flask.Markup(strings['page_not_found_text']) % _link(
+                config.lang_code, 'Citation Hunt')
 
     strings.setdefault('instructions_goal', '')
     strings.setdefault('instructions_details', '')
@@ -41,6 +38,13 @@ def _preprocess_variables(config, strings):
                     flask.Markup('<b>' + strings['button_wikilink'] + '</b>'),
                     flask.Markup('<b>' + strings['button_next'] + '</b>'),
                     beginners_hint_link)
+
+    strings.setdefault('footer', '')
+    if strings['footer']:
+        # We replace the URLs in the template itself
+        strings['footer'] = flask.Markup(strings['footer']) % (
+            _link('%s', 'Tools Labs'),
+            _link('%s', 'translatewiki.net'))
     return strings
 
 def get_localized_strings(config, lang_code):
