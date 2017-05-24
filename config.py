243acee10107adc7b0aacfc774cff3a35c131bc6
Henrique de Andrade <henriquedeandrade@gmail.com>
2017-05-24 10:35:44 -0300

    adding more configurations to pt

--- config.py
+++ config.py
@@ -150,24 +150,32 @@ _LANG_CODE_TO_CONFIG = dict(
     
     pt = dict(
         lang_name = 'Português',
-        lang_dir = 'lpt',
+        lang_dir = 'ltr',
         database = 'ptwiki_p',
         wikipedia_domain = 'pt.wikipedia.org',
         citation_needed_category = '!Artigos_que_carecem_de_fontes',
         beginners_link = 'https://pt.wikipedia.org/wiki/ Ajuda:Tutorial/Referência',
         beginners_link_title = ' Ajuda:Tutorial/Referência',
-        lead_section_policy_link = 'https://pt.wikipedia.org/wiki/WP:Wikipédia:Políticas_e_recomendações',
-        lead_section_policy_link_title = 'WP:Políticas_e_recomendações',
+        lead_section_policy_link = 'https://pt.wikipedia.org/wiki/Wikipedia:INTROREF',
+        lead_section_policy_link_title = 'WP:INTROREF',
+
+        accept_language = [
+            'pt',
+            'pt-BR',
+        ],
+
+    category_name_regexps_blacklist = [
+        '^!',
+    ],
 
         citation_needed_templates = [
-            'Carece de fontes',
+            'Carece_de_fontes',
             'Sem-fontes',
             'Sem-fontes-bpv',
             'Sem-fontes-sobre',
         ],
         citation_needed_template_name = 'Carece de fontes',
         hidden_category = '!Categorias_ocultas',
-    
     ),
     
     fr = dict(
