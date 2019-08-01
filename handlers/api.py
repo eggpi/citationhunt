6f2339c8e9af0416e3c56765698087d7df0291b2
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2019-08-01 09:53:02 +0100

    Actually include api.py

---
+++ handlers/api.py
@@ -0,0 +1,22 @@
+from . import database
+from utils import *
+from .common import *
+
+@validate_lang_code
+def snippets_in_articles(lang_code):
+    cfg = flask.g._cfg
+    try:
+        page_ids = flask.request.args.getlist('page_id', type=int)
+    except ValueError:
+        return flask.jsonify(error = 'Invalid request')
+    if not page_ids:
+        return flask.jsonify(error = 'Invalid request')
+    result = database.get_snippets_in_articles(
+        lang_code, page_ids, cfg.api.max_returned_snippets)
+    return flask.jsonify({
+        page_id: [
+            flask.url_for('citation_hunt',
+                id = snippet_id, lang_code = lang_code)
+            for snippet_id in snippet_ids
+        ]
+    for page_id, snippet_ids in result.items()})
