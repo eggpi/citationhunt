93a81f75657def510f41fe3d086c7fad1428a627
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2019-08-01 20:17:48 +0100

    Hand out full URLs in our API.

--- handlers/api.py
+++ handlers/api.py
@@ -16,7 +16,7 @@ def snippets_in_articles(lang_code):
     return flask.jsonify({
         page_id: [
             flask.url_for('citation_hunt',
-                id = snippet_id, lang_code = lang_code)
+                id = snippet_id, lang_code = lang_code, _external = True)
             for snippet_id in snippet_ids
         ]
     for page_id, snippet_ids in result.items()})
