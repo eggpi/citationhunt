1843e34bba0343cd3600f3c8934ae29b4b365554
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2020-03-13 14:30:37 +0000

    Extend chstrings smoke test a little more.

--- chstrings/chstrings_test.py
+++ chstrings/chstrings_test.py
@@ -7,8 +7,13 @@ class CHStringsTest(unittest.TestCase):
     @classmethod
     def add_smoke_test(cls, cfg):
         def test(self):
-            # We just want to see if this will blow up
-            chstrings.get_localized_strings(cfg, cfg.lang_code)
+            # We just want to see if this will blow up. Use the fallback
+            # lang_tag across all tests.
+            lang_tag = cfg.lang_code
+            if cfg.accept_language:
+                lang_tag = cfg.accept_language[-1]
+            self.assertNotEqual({},
+                chstrings.get_localized_strings(cfg, lang_tag))
         name = 'test_' + cfg.lang_code + '_smoke_test'
         setattr(cls, name, test)
 
