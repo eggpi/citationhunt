be9ec3c24322156785301701b499cc3ea3576160
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2020-03-13 13:44:02 +0000

    Interpolate-then-format instructions_details string.
    When the beginners_link contains URL-encoded characters, Python gets
    confused and things those are invalid formatting characters (e.g. %C),
    which throws an exception.

--- chstrings/__init__.py
+++ chstrings/__init__.py
@@ -55,7 +55,6 @@ def _preprocess_variables(config, strings):
                     flask.Markup('<b>' + strings['button_wikilink'] + '</b>'),
                     flask.Markup('<b>' + strings['button_next'] + '</b>'),
                     beginners_hint_link)
-
     else:
         # Support beginner's link, optionally.
         if hasattr(config, 'beginners_link'):
@@ -63,10 +62,10 @@ def _preprocess_variables(config, strings):
         else:
             link_start = link_end = ''
         strings['instructions_details'] = flask.Markup(
-                strings['instructions_details'].format(
-                    link_start = link_start, link_end = link_end) % (
-                        flask.Markup('<b>' + strings['button_wikilink'] + '</b>'),
-                        flask.Markup('<b>' + strings['button_next'] + '</b>')))
+                (strings['instructions_details'] % (
+                    flask.Markup('<b>' + strings['button_wikilink'] + '</b>'),
+                    flask.Markup('<b>' + strings['button_next'] + '</b>'))).format(
+                        link_start = link_start, link_end = link_end))
 
     strings.setdefault('footer', '')
     if strings['footer']:
