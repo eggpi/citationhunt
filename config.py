3fb07b7721ae0f84c2755e9e3c6564fcd45441a6
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2021-01-15 08:30:31 +0000

    Replace category and add a new template for mkwiki.

--- config.py
+++ config.py
@@ -838,9 +838,10 @@ _LANG_CODE_TO_CONFIG = dict(
         reliable_sources_link = 'https://mk.wikipedia.org/wiki/%D0%92%D0%B8%D0%BA%D0%B8%D0%BF%D0%B5%D0%B4%D0%B8%D1%98%D0%B0:%D0%9F%D1%80%D0%BE%D0%B2%D0%B5%D1%80%D0%BB%D0%B8%D0%B2%D0%BE%D1%81%D1%82',
         lead_section_policy_link = '',
         lead_section_policy_link_title = '',
-        citation_needed_category = 'Сите_статии_со_непотврдени_изјави',
+        citation_needed_category = 'Сите_статии_без_извори',
         citation_needed_templates = [
             'Се бара извор',
+            'Без извори',
         ],
         hidden_category = 'Скриени_категории',
     )
